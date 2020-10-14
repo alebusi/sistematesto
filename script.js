76205356bde3e4932f1efa5e27588592b4a601e3
alebusi <allebusi@gmail.com>
2020-10-14 17:57:50 +0200

    Update and rename plagio.js to script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function generaTesto() {
+  testo=document.getElementById("testoIn").value;
+  testo=sistemaTesto(testo);
+  document.getElementById("testoOut").innerHTML = testo;
+}
+
+function sistemaTesto(testo) {
+  testo = testo.replace(/à/ig,"ah");
+  testo = testo.replace(/ì/ig,"ih");
+  testo = testo.replace(/ù/ig,"uh");
+  testo = testo.replace(/ò/ig,"oh");
+  testo = testo.replace(/è/ig,"eh");
+  testo = testo.replace(/é/ig,"eh");
+  return testo;
+}
