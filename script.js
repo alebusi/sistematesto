a899c9bc7a60cffc47f526ae8bb452bb96d5dba7
alebusi <allebusi@gmail.com>
2020-11-19 15:09:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ function generaTesto() {
   testo=sistemaTesto(testo);
   //
   posFinale=estraiPos(testo,pos);
-  alert("pos finale 1"+posFinale);
   testoCut=testo.substr(0,posFinale+1);  
   document.getElementById("testoOut").value = testoCut;
   testo=testo.substr(posFinale+1);
   // due
   posFinale=estraiPos(testo,pos);
-  alert("pos finale 2"+posFinale);
   testoCut=testo.substr(0,posFinale+1);  
   document.getElementById("testoOut2").value = testoCut;
   testo=testo.substr(posFinale+1);
