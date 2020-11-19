9bd7a2c0d28b4b68c97f489a4806386b8a94f6e5
alebusi <allebusi@gmail.com>
2020-11-19 12:38:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+pos=7100;
+
 function generaTesto() {
   //testo="<html><head><title>Altavoce</title></head><body><div>";
   testo=document.getElementById("testoIn").value;
   testo=sistemaTesto(testo);
+  //
+  posFinale=estraiPos(testo,pos);
+  testoCut=testo.substr(0,posFinale);  
+  document.getElementById("testoOut").value = testoCut;
+  testo=testo.substr(posFinale+1);
+  // due
+  posFinale=estraiPos(testo,pos);
+  testoCut=testo.substr(0,pos);  
+  document.getElementById("testoOut2").value = testoCut;
+  testo=testo.substr(posFinale+1);
+  // tre
+  posFinale=estraiPos(testo,pos);
+  testoCut=testo.substr(0,pos);  
+  document.getElementById("testoOut3").value = testoCut;
+  testo=testo.substr(posFinale+1);
+  // quattro
+  posFinale=estraiPos(testo,pos);
+  testoCut=testo.substr(0,pos);  
+  document.getElementById("testoOut4").value = testoCut;
+  testo=testo.substr(posFinale+1);  
+  
   //testo=testo+"</div></body></html>";
-  document.getElementById("testoOut").value = testo;
 }
 
 function sistemaTesto(testo) {
@@ -14,4 +36,9 @@ function sistemaTesto(testo) {
   testo = testo.replace(/è/ig,"eh");
   testo = testo.replace(/é/ig,"eh");
   return testo;
+}
+
+function estraiPos(testo, pos) {
+  posFinale=testo.indexOf(".",pos);
+  return posFinale;
 }
