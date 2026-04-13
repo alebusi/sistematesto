e8a686742ea28f92aa1ff13cc756b08f1653b673
alebusi <allebusi@gmail.com>
2026-04-13 10:56:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var targetFileId1 = "16CbLxt-lWdQyXTM9IVz_KBC1-7GmdpohtnG2ZzStn4w";
+var targetFileId2 = "1sRR7l35CNwreLbRkBWkniFKrGBVlBKXwVFojfQYYF3Y";
+var targetFileId3 = "1vbnmWGUAjFABMK3GAsvNMHGP_lLnSibgtSblDpxrQag";
+var targetFileId4 = "1d2XRPVuScck8BJpPdXdt2bzgQq_BIg9Pk7afA1tgxaU";
 pos=7100;
 
 function generaTesto() {
@@ -12,6 +16,30 @@ function generaTesto() {
     document.getElementById("testoOut4").value = "";
   }
   else {
+    posFinale=estraiPos(testo,pos);
+    testoCut=testo.substr(0,posFinale+1);  
+    files = DriveApp.getFileById(targetFileId1); 
+    files.setContent(testoCut);
+    testo=testo.substr(posFinale+1);
+    // due
+    posFinale=estraiPos(testo,pos);
+    testoCut=testo.substr(0,posFinale+1);  
+    files = DriveApp.getFileById(targetFileId2); 
+    files.setContent(testoCut);
+    testo=testo.substr(posFinale+1);
+    // tre
+    posFinale=estraiPos(testo,pos);
+    testoCut=testo.substr(0,posFinale+1);  
+    files = DriveApp.getFileById(targetFileId3); 
+    files.setContent(testoCut);
+    testo=testo.substr(posFinale+1);
+    // quattro
+    posFinale=estraiPos(testo,pos);
+    testoCut=testo.substr(0,posFinale+1);  
+    files = DriveApp.getFileById(targetFileId4); 
+    files.setContent(testoCut);
+    testo=testo.substr(posFinale+1);
+    /*
     posFinale=estraiPos(testo,pos);
     testoCut=testo.substr(0,posFinale+1);  
     document.getElementById("testoOut").value = testoCut;
@@ -31,6 +59,7 @@ function generaTesto() {
     testoCut=testo.substr(0,posFinale+1);  
     document.getElementById("testoOut4").value = testoCut;
     testo=testo.substr(posFinale+1);
+    */
   }  
   //testo=testo+"</div></body></html>";
 }
